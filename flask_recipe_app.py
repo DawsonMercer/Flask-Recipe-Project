62cbc276bc501618d28858a8bbec199a8194d75e
DawsonMercer <dwem52@mun.ca>
2022-04-10 16:00:39 -0230

    Added log-in, error, index, random, viewall.

--- flask_recipe_app.py
+++ flask_recipe_app.py
@@ -1,9 +1,45 @@
 import csv
 import os
+import random
 from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
+#venv\Scripts\activate
+#$env:FLASK_APP = "flask_recipe_app"
+# $env:FLASK_ENV = "development"
+#flask run
+
+
 @app.route("/")
 def main_page():
     return render_template("index.html")
+
+
+@app.route("/view-all")
+def view_all():
+    return render_template("viewAll.html")
+
+@app.route("/recipe/<string:recipe_name>")
+def recipe(recipe_name):
+
+    recipe_list = ["applepie", "smoothie", "salad"]
+    # todo if recipe_name exists in csv, display recipe. else error
+
+    if(recipe_name in recipe_list):
+        return render_template("recipe.html", recipe_name=recipe_name)
+    else:
+        return render_template("error.html")
+
+
+@app.route("/pick-random")
+def pick_random():
+    recipe_list = ["applepie", "smoothie", "salad"]
+
+    # todo add random page html
+    random_recipe_name = random.choice(recipe_list)
+    return render_template("random.html", recipe_list=recipe_list)
+
+@app.route("/log-in")
+def log_in():
+    return render_template("login.html")
